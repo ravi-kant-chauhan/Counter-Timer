b9fbde0c4a0685de7e8a257d1c9d93a268810e66
Ravi <ravkchauhan1aip@gmail.com>
2023-03-17 12:46:03 +0530

    js commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const daysElement = document.querySelector(".days");
+const hoursElement = document.querySelector(".hours");
+const minutesElement = document.querySelector(".minutes");
+const secondsElement = document.querySelector(".seconds");
+const heading = document.querySelector("h1")
+const counterTimer = document.querySelector(".counterTimer");
+
+const second = 1000,
+    minute = 60 * second, 
+    hour = 60 * minute,
+    day = 24 * hour;
